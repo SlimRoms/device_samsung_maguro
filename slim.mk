e351a73ba3b0e341e40bb78054d90aa2ad7ebdc0
kufikugel <kufikugel@googlemail.com>
2012-11-29 03:13:34 +0100

    maguro touches 4.2.1
    PatchSet 2
    
    added widivine and nxp in dependencies
    
    old tab clenup
    
    PatchSet 3
    
    change release name to codename maguro

---
+++ slim.mk
@@ -0,0 +1,44 @@
+# Boot animation
+TARGET_BOOTANIMATION_NAME := vertical-720x1280
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/maguro/full_maguro.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/slim/config/gsm.mk)
+
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+# Inherit device settings
+$(call inherit-product, vendor/slim/config/common_nexus.mk)
+
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.camera.res=5MP
+
+#copy 00check
+#PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
+
+#copy vold.fstab
+PRODUCT_COPY_FILES += \
+    device/samsung/maguro/vold.fstab:system/etc/vold.fstab
+
+# Release name
+PRODUCT_RELEASE_NAME := maguro
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_FINGERPRINT="google/yakju/maguro:4.2.1 JOP40D eng.ar.20121128.230418 release-keys"
+PRIVATE_BUILD_DESC="yakju-user 4.2.1 JOP40D 230418 release-keys"
+
+PRODUCT_NAME := slim_maguro
+PRODUCT_DEVICE := maguro
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Galaxy Nexus
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RESTRICT_VENDOR_FILES := false
